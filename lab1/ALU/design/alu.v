4a959addd62b675779b92c300a6d67b89355707d
minervazz <96127085+StellaVesperi@users.noreply.github.com>
2023-04-18 10:48:06 +0800

    update lab0-2

--- lab1/ALU/design/alu.v
+++ lab1/ALU/design/alu.v
@@ -1,28 +1,58 @@
-module alu #(parameter WIDTH = 6)   (
+module alu #(parameter WIDTH = 6) ( 
     input [WIDTH - 1:0] a, b,       // 操作数
     input [2:0] f,                  // 功能
     output reg [WIDTH - 1:0] y,     // 运算结果
-    output z                        // 溢出标志of
+    output reg z                    // 溢出标志of
 );
-    assign z = (y == {WIDTH{1'h0}}); // 如果有溢出，则为1，否则为0
+
     always @(*) begin
         case (f)
-            4'b0000:
-                y = a + b;
+            4'b0000:  
+            begin       
+                    y = a + b;
+                    if ((a[5] == b[5]) && (y[5] != a[5]))
+                        z = 1'b1;
+                    else
+                        z = 1'b0;
+            end
             4'b0001:
-                y = a - b;
+            begin     
+                    y = a - b;
+                    if ((a[5] != b[5]) && (y[5] != a[5])) 
+                    else
+                        z = 1'b0;
+            end
             4'b0010:
-                y = (a == b) ? 6'b000001 : 6'b0; // 若满足a等于b，则为1，否则为0
+            begin      
+                    y = (a == b) ? 6'b000001 : 6'b0; // 若满足a等于b，则为1，否则为0
+                    z = 1'b0;
+            end
             4'b0011:
-                y = (a < b) ? 6'b000001 : 6'b0;  // 若满足a小于b，则为1，否则为0
+            begin      
+                    y = (a < b) ? 6'b000001 : 6'b0;  // 若满足a小于b，则为1，否则为0
+                    z = 1'b0;
+            end
             4'b0101:
-                y = a & b;
+                begin
+                    y = a & b;
+                    z = 1'b0;
+                end
             4'b0110:
-                y = a | b;
+                begin
+                    y = a | b;
+                    z = 1'b0;
+                end
             4'b0111:
-                y = a ^ b;
+                begin
+                    y = a ^ b;
+                    z = 1'b0;
+                end
             default:
-                y = {WIDTH{1'h0}};               // 未定义功能
+                begin
+                    y = {WIDTH{1'h0}};               // 未定义功能
+                    z = 1'b0;
+                end
         endcase
     end
 endmodule
+
